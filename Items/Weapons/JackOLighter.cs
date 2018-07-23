29b7f46ee084f2a900c70c506f52e1b81d2ebf75
Kooyah <40721501+Kooyah@users.noreply.github.com>
2018-07-23 11:21:53 -0700

    Add files via upload

---
+++ Items/Weapons/JackOLighter.cs
@@ -0,0 +1,70 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NEF.Items.Weapons
+{
+	public class JackOLighter : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Jack O' Lighter");
+	
+			Tooltip.SetDefault("Shoots a constant stream of fire"
+				+ "\nUses gel for ammo");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 10;
+			item.ranged = true;
+			item.width = 48;
+			item.height = 20;
+			item.useTime = 6;
+			item.useAnimation = 30;
+			item.useStyle = 5;
+			item.noMelee = true;
+			item.knockBack = 0;
+			item.value = 15000;
+			item.rare = 0;
+			item.UseSound = SoundID.Item34;
+			item.autoReuse = true;
+			item.shoot = mod.ProjectileType("PumpkinFire");
+			item.reuseDelay = 6;
+			item.shootSpeed = 10f;
+			item.useAmmo = AmmoID.Gel;
+		}
+
+		public override bool ConsumeAmmo(Player player)
+		{
+            		return !(player.itemAnimation < item.useAnimation - 2);
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 55f;
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+			return true;
+		}
+
+		public override void AddRecipes()
+		
+		{
+	ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Pumpkin, 30);
+			recipe.AddIngredient(ItemID.Wood, 16);
+			recipe.AddIngredient(ItemID.IllegalGunParts);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+
+			recipe.AddRecipe();
+
+		
+		}
+	
+	}
+}
